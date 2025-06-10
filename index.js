d5208ce51a420f648bbfce429b32b093c14ad67b
GeekyNaved <navedahmed040@gmail.com>
2025-06-10 16:29:21 +0530

    adding env variable code

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 const app = express();
 const PORT = 5000;
+
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('This is Home Page');
+  res.send(`This is Home Page with env API_KEY=${process.env.API_KEY}`);
 });
 
 app.post('/login-action', (req, res) => {
